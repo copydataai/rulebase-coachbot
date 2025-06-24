715ed4347ecb5d276a6e601fb36c8a5a8435b226
Jose Sanchez <engineer@copydataai.com>
2025-06-24 08:32:46 +0100

    chore: add transformers & charts deps

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    webpack: (config) => {
+        // See https://webpack.js.org/configuration/resolve/#resolvealias
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            "sharp$": false,
+            "onnxruntime-node$": false,
+        }
+        return config;
+    },
 };
 
 export default nextConfig;
